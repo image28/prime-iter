6eac4466e5666bf9474599a83d76fac648773cd7
Kevin Macey <image28@slingshot.co.nz>
2020-08-07 17:34:12 +1200

    Merged decoder in prime encoder file
    Working on making a standard for all my code with the IFDEFS,
    this code hasn't been checked, tested or compiled yet, going out!

---
+++ prime-store.c
@@ -0,0 +1,123 @@
+#ifdef CINC || MAIN 
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <sys/types.h>
+    #include <string.h>
+
+    #define _GNU_SOURCE
+    #define MAP_HUGE_1GB (30 << MAP_HUGE_SHIFT)
+
+    #define MAXSIZE 1000000000
+
+    // https://stackoverflow.com/questions/15906600/what-is-the-most-efficient-way-to-store-prime-numbers-in-an-array
+    //1, 7, 11, 13, 17, 19, 23 or 29
+    // prime/30 = byte in array
+    // prime-(30*bitpos) = bit in array
+
+    int encode(char outfile, u_int64_t *byte, u_int8_t *bitpos, u_int8_t *primearray);
+    int decode(char outfile, u_int64_t *byte, u_int8_t *bitpos, u_int8_t *primearray);
+#endif
+
+#ifdef MAIN
+    int main(int argc, char *argv[])
+    {
+        u_int64_t byte=1;
+        u_int8_t bitpos[8] = {1,7,11,13,17,19,23,29};
+        u_int8_t *primearray;
+        
+        #ifdef ENCODE
+            encode(argv[1],&byte,&bitpos,&primearray);
+        #elif DECODE
+            decode(argv[1],&byte,&bitpos,&primearray);
+        #endif
+
+        return 0;
+    }
+#endif
+
+#ifdef ENCODE
+    int encode(char outfile, u_int64_t *byte, u_int8_t *bitpos, u_int8_t *primearray)
+    {
+        u_int8_t mask=1;
+        u_int8_t prev=0;
+        u_int64_t filesize=0;
+        primearray=calloc(MAXSIZE,1);
+
+        do
+        {
+        prev=*(byte);
+        scanf("%llu\n",byte);
+
+        #ifdef INFO
+            printf("%llu\n",*(byte));
+        #endif
+    
+            byte=byte-((byte/30)*30);
+
+            for(i=0; i < 8; i++)
+            {   
+                if ( bitpos[i] == byte)
+                {
+                    *(primearray+filesize)=*(primearray+filesize)+(mask<<*(bitpos+i));
+                    mask=1;
+                    i=8;
+                }
+            }
+            filesize++;
+        }while ( ( filesize < MAXSIZE ) && ( byte != prev ) && ( byte != 0 ) );
+
+    #ifdef WRITE
+        FILE *output;
+        output=fopen(outfile,"wb+");
+        fwrite(primearray,1,filesize,output);
+        fclose(output);
+    #endif
+
+        return(0);
+    }
+#endif
+
+#ifdef DECODE
+    int decode(char outfile, u_int64_t *byte, u_int8_t *bitpos, u_int8_t *primearray)
+    {
+        u_int64_t filesize=0;
+        u_int64_t position=0;
+
+        if ( ( input=fopen(infile,"rb+")) == NULL ) exit(-1);
+        fseek(input,0L,SEEK_END);
+        filesize=ftell(input);
+        fseek(input,0L,SEEK_SET);
+
+    #ifdef WRITE
+        FILE *output;
+        output=fopen(outfile,"wb+");
+    #endif
+
+        if ( filesize < MAXSIZE )
+        {
+            primearray=calloc(filesize,1);
+            fread(primearry,1,filesize,input);
+        }else{
+            primearray=calloc(MAXSIZE,1);
+        }
+
+        do{
+            for(i=0; i < 8; i++)
+            {
+                byte=*(bitpos+i)+position*30);
+                #ifdef DISPLAY
+                    printf("%llu\n",byte);
+                #elif WRITE
+                    fwrite(byte,1,1,output);
+                #endif
+            }
+            position++;
+        }while ( ( position < filesize ) && ( position < MAXSIZE ) );
+        
+    #ifdef WRITE
+        fclose(output);
+    #endif
+
+        return(0);
+    }
+#endif
