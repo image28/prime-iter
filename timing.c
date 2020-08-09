c6254df5ae5c2ee66a5f0f8d541b7328842b7006
Kevin Macey <image28@slingshot.co.nz>
2020-08-09 12:05:09 +1200

    Update timing.c
    Fixed a few typos :) *Lol*

--- timing.c
+++ timing.c
@@ -1,8 +1,21 @@
+#ifdef ALLIN
+    #define COMPILER "gcc "
+    #define MAIN    1
+    #define TIMESTAMP 1
+    #define GRAPH 1
+    #define AVXPRINT 1
+    #define AVXMATH 1
+    #define INFO 1
+    #define BENCH 1
+    #define COMPILE 1
+#endif
+
 #if defined MAIN || defined CINC
 #include <sys/types.h>
 #include <immintrin.h>
 #include <inttypes.h>
 #include "gnuplot_pipes.h"
+#include <stdlib.h>
 #endif
 
 #ifdef MATHMACROS
@@ -18,7 +31,7 @@ void timestamp(u_int64_t *result)
 
     gettimeofday(&tv,NULL);
     // https://graphics.stanford.edu/~seander/bithacks.html#IntegerLog10
-    *(result)=(tv.tv_sec*((tv.tv_usec >= 1000000) ? 6 : (tv.tv_usec >= 100000) ? 5 : (v >= 10000) ? 4 : 
+    *(result)=(tv.tv_sec*((tv.tv_usec >= 1000000) ? 6 : (tv.tv_usec >= 100000) ? 5 : (tv.tv_usec >= 10000) ? 4 : 
     (tv.tv_usec >= 1000) ? 3 : (tv.tv_usec >= 100) ? 2 : (tv.tv_usec >= 10) ? 1 : 0))+tv.tv_usec;
 }
 #endif
@@ -43,6 +56,8 @@ inline __m256i _mm256_div_epi16 (const __m256i va, const int b)
 #ifdef COMPILE
 void compile(char *cmd, char *defines)
 {
+    char compileline[65535];
+
     strcpy(compileline, COMPILER);
     strcat(compileline, cmd);
     strcat(compileline, ".c ");
@@ -50,51 +65,56 @@ void compile(char *cmd, char *defines)
     strcat(compileline, " -o ");
     strcat(compileline, cmd);
 
-    exec(compileline);
+    system(compileline);
 }
 #endif
 
 #ifdef BENCH
-void benchmark(char *cmd, int runs, char *defines)
+void benchmark(char *cmd, int runs, char *defines, u_int64_t *results, u_int64_t *fastest, u_int64_t *slowest)
 {
     __m256i total=_mm256_set_epi64x(0,0,0,0);
     __m256i average=_mm256_set_epi64x(0,0,0,0);
-    u_int64_t slowest=0;
-    u_int64_t fastest=18446744073709551615ULL;
     __m256i temp=_mm256_set_epi64x(0,0,0,0);
     
     u_int64_t start=0;
     u_int64_t stop=0;
     u_int64_t result=0;
     char nullpointer='\0';
+    int i=0;
 
 #ifdef COMPILE
     if ( *(cmd+(strlen(cmd)-1)) == 'c' )
     {
-        compile(cmd,nullpointer);
+        compile(cmd,&nullpointer);
     }
 #endif 
 
-    for(i=0; i < runs; i++)
+    for(i=0; i < runs; i=i+2)
     {
 
         timestamp(&start);
-        exec(cmd);
+        system(cmd);
         timestamp(&stop);
 
         result=stop-start;
+        if ( *(results+runs-1) == 0 )
+        {
+            *(results+i*2+1)=result;
+        }else{
+            *(results+i)=result;
+        }
 
         temp=_mm256_set_epi64x(0,0,0,result);
         total=_mm256_add_epi64(total,temp);
 
-        if ( result < fastest)
+        if ( result < *(fastest))
         {
-            fastest=result;
+            *(fastest)=result;
         }
 
-        if ( result > slowest)
+        if ( result > *(slowest))
         {
-            slowest=result;
+            *(slowest)=result;
         }
 
     }
@@ -112,7 +132,7 @@ void benchmark(char *cmd, int runs, char *defines)
     #endif
 
 #ifdef COMPILE
-    if ( defines != '\0' )
+    if ( *(defines) != '\0' )
     {
         #ifdef INFO
             printf("Recompiling with different compile defines");
@@ -122,40 +142,62 @@ void benchmark(char *cmd, int runs, char *defines)
             printf("Benchmarking with new defines");
         #endif
         *(defines)='\0';
-        benchmark(cmd,runs,nullpointer);  
+        benchmark(cmd, runs, &nullpointer, results, fastest, slowest);  
     }
 #endif
 
 }
+#endif
 
 #ifdef GRAPH
+/*
 void plotpoint()
 {
     // Plot points
     gnuplot_setstyle(h1, "points") ;
     gnuplot_plot1d_var2(h1, dp, 1,"") ;
-}
+}*/
 
+void plotpoints(gnuplot_ctrl *h1, dpoint *dp, int runs)
+{
+    // Plot points
+    gnuplot_setstyle(h1, "points") ;
+    gnuplot_plot1d_var2(h1, dp, runs,"") ;
+    
+}
+/*
 void plotline()
 {
     // Line for average
     gnuplot_setstyle(h1, "lines") ;
     gnuplot_plot_slope(h1, 2.0, 0.0, "") ;
-}
+}*/
 #endif
 
 #ifdef MAIN
 int main(int argc, char *argv[])
 {
+    u_int64_t slowest=0;
+    u_int64_t fastest=18446744073709551615ULL;
+    u_int64_t *results;
+    u_int64_t runs=0;
+    gnuplot_ctrl *h1;
+
+    runs=strtol(argv[2],NULL,10);
+    results=calloc(runs*2,8);
+
 #ifdef GRAPH
     h1 = gnuplot_init() ;
 #endif
 
-    benchmark(argv[1],strtol(argv[2],NULL,10), argv[3]);
+    benchmark(argv[1],runs,argv[3], results, &fastest, &slowest);
+    plotpoints(h1, (struct _DPOINT_ *)results,runs);
 
 #ifdef GRAPH
     gnuplot_close(h1) ;
 #endif
+
+    free(results);
 
     return(0);
 }
